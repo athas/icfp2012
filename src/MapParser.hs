36b25cb433f54ff0723dbe2c273c1d01aaf98437
Troels Henriksen <athas@sigkill.dk>
2012-07-13 15:36:22 +0200

    More efficient map representation.

--- src/MapParser.hs
+++ src/MapParser.hs
@@ -4,7 +4,6 @@ module MapParser
   where
 
 import Control.Monad
-import qualified Data.Map as M
 import Data.Maybe
 import MineMap
 
@@ -16,20 +15,21 @@ parseChar '*' = return Rock
 parseChar '#' = return Wall
 parseChar '\\' = return Lambda
 parseChar '.' = return Earth
-parseChar 'L' = return $ LambdaLift Closed
-parseChar 'O' = return $ LambdaLift Open
+parseChar 'L' = return $ Lift Closed
+parseChar 'O' = return $ Lift Open
 parseChar 'R' = return Robot
 parseChar c = fail $ "Invalid character in map: '" ++ [c] ++ "'"
 
 parseMap :: String -> MapParser MineMap
-parseMap = mkMap <=< foldM combine (Nothing, M.empty)
+parseMap = mkMap <=< foldM combine (Nothing, newMap (0,0))
            . concatMap mkpos . zip [1..] . reverse . lines
   where mkpos (y,l) = zipWith (\x c -> ((x,y), c)) [1..] l
-        combine (rp, m) (p, c) = do c' <- parseChar c
-                                    let m' = M.insert p c' m
-                                    case c' of
-                                      Robot | isJust rp -> fail "Two robots in map"
-                                            | otherwise -> return (Just p, m')
-                                      _ -> return (rp, m')
+        combine (rp, m) (p, c) = do
+          c' <- parseChar c
+          let m' = setCell m p c'
+          case c' of
+            Robot | isJust rp -> fail "Two robots in map"
+                  | otherwise -> return (Just p, m')
+            _ -> return (rp, m')
         mkMap (Nothing, _) = Left "No robot in map"
-        mkMap (Just p, m) = Right $ MineMap p m
+        mkMap (Just _, m) = Right m
--- src/MineMap.hs
+++ src/MineMap.hs
@@ -3,23 +3,88 @@ module MineMap
   , LiftState(..)
   , Cell(..)
   , MineMap(..)
+  , newMap
+  , isEarth
+  , isRobot
+  , isWall
+  , isRock
+  , isLambda
+  , isLift
+  , isEmpty
+  , setCell
   , Action(..)
   , Route
   , routeToString
   ) where
 
 import qualified Data.Map as M
+import qualified Data.Set as S
 
 type Pos = (Int, Int)
 
 data LiftState = Open | Closed
                  deriving (Eq, Ord, Show)
 
-data Cell = Earth | Robot | Wall | Rock | Lambda | LambdaLift LiftState | Empty
+data Cell = Earth | Robot | Wall | Rock | Lambda | Lift LiftState | Empty
             deriving (Eq, Ord, Show)
 
-data MineMap = MineMap Pos (M.Map Pos Cell)
+data MineMap = MineMap { robot :: Pos
+                       , cells :: M.Map Pos Cell
+                       , lifts :: S.Set Pos
+                       , rocks :: S.Set Pos
+                       , lambdas :: S.Set Pos
+                       }
              deriving (Eq, Ord, Show)
+
+newMap :: Pos -> MineMap
+newMap p = MineMap p (M.singleton p Robot) S.empty S.empty S.empty
+
+cellProp :: (Cell -> Bool) -> Bool -> MineMap -> Pos -> Bool
+cellProp f d m p = maybe d f $ M.lookup p $ cells m
+
+isWall :: MineMap -> Pos -> Bool
+isWall = cellProp (==Wall) False
+
+isRobot :: MineMap -> Pos -> Bool
+isRobot = cellProp (==Robot) False
+
+isEarth :: MineMap -> Pos -> Bool
+isEarth = cellProp (==Earth) False
+
+isLift :: MineMap -> Pos -> Bool
+isLift = cellProp f False
+  where f (Lift _) = True
+        f _        = False
+
+isRock :: MineMap -> Pos -> Bool
+isRock = cellProp (==Rock) False
+
+isEmpty :: MineMap -> Pos -> Bool
+isEmpty = cellProp (==Empty) False
+
+isLambda :: MineMap -> Pos -> Bool
+isLambda = cellProp (==Lambda) False
+
+setCell :: MineMap -> Pos -> Cell -> MineMap
+setCell m p c = register m' p c
+  where m' = maybe m (unregister m p) $ M.lookup p $ cells m
+
+unregister :: MineMap -> Pos -> Cell -> MineMap
+unregister m p c = case c of
+                     Lift _ -> m' { lifts = S.delete p (lifts m') }
+                     Rock   -> m' { rocks = S.delete p (rocks m') }
+                     Lambda -> m' { lambdas = S.delete p (lambdas m') }
+                     _      -> m'
+  where m' = m { cells = M.delete p (cells m) }
+
+register :: MineMap -> Pos -> Cell -> MineMap
+register m p c = case c of
+                   Lift _ -> m' { lifts = S.insert p (lifts m') }
+                   Rock   -> m' { rocks = S.insert p (rocks m') }
+                   Lambda -> m' { lambdas = S.insert p (lambdas m') }
+                   Robot  -> m' { robot = p }
+                   _      -> m'
+  where m' = m { cells = M.insert p c (cells m) }
 
 data Action = MoveUp | MoveDown | MoveLeft | MoveRight | Wait | Abort
 
