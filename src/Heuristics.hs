0aede198a8edcefd58c9804c905003f53d62cee4
Troels Henriksen <athas@sigkill.dk>
2012-07-14 13:37:46 +0200

    Move rocks.

--- src/Heuristics.hs
+++ src/Heuristics.hs
@@ -23,14 +23,17 @@ data Heuristic = Heuristic {
 
 runHeuristic :: MineMap -> Heuristic -> Route
 runHeuristic m h = reverse $ steps $ maximumBy (comparing score) $
-                   concat $ run $ stateFromMap m
-  where run sim =
+                   run $ stateFromMap m
+  where run :: SimState -> [SimState]
+        run sim =
           let m' = mineMap sim
               dests = case nextLambda h m' of
                         [] -> S.toList $ lifts m'
                         ls -> take 3 ls
-              routes = concatMap (take 3 . routeTo h m') dests
-          in if finished sim || all null routes then return [sim]
+              routes = concatMap (take 3 . filter (not . null) . routeTo h m') dests
+          in -- trace ("trying lambdas " ++ show dests) $
+             -- trace ("with routes " ++ show routes) $
+             if finished sim || null routes then [sim]
              else concatMap (run . walk sim) routes
 
 dumbHeuristic :: Heuristic
@@ -64,7 +67,7 @@ pathsTo = genRoutes [[]]
 
 pathTo :: MineMap -> Pos -> Pos -> Route
 pathTo m from to = reverse $ go (M.singleton from []) [(from, [])]
-  where go seen []     = fromMaybe [] (M.lookup to seen)
+  where go seen []     = fromMaybe [Abort] (M.lookup to seen)
         go seen ((p,path):ns) =
           let (seen', ns') = foldl check (seen, ns) $ neighbors p path
           in go seen' ns'
@@ -72,6 +75,11 @@ pathTo m from to = reverse $ go (M.singleton from []) [(from, [])]
                                       Nothing -> (M.insert p path seen, (p,path):ns)
                                       Just path' | length path >= length path' -> (seen, ns)
                                                  | otherwise -> (M.insert p path seen, (p,path):ns)
-        neighbors p path = filter (isWalkable m . fst) $
+        neighbors p path = filter walkable $
                            map (\a -> (move p a, a:path))
                            [MoveUp,MoveDown,MoveLeft,MoveRight]
+        walkable (p@(x,y), path) = isWalkable m p || (isRock m p && rockmovable)
+          where rockmovable = case path of
+                                MoveRight:_ -> isEmpty m (x+1,y)
+                                MoveLeft:_ -> isEmpty m (x-1,y)
+                                _  -> False
